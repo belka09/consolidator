475347167166bfdd199ca955f57a010d309fbb7a
Dzmitry Zimnitski <dmitryft4@gmail.com>
2025-03-23 19:11:52 +0100

    UI fixes

--- src/app/travelers/components/summary/summary.component.ts
+++ src/app/travelers/components/summary/summary.component.ts
@@ -1,12 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { Component, input } from '@angular/core';
+import { Component, computed, input, signal } from '@angular/core';
 
 @Component({
   selector: 'app-summary',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.scss',
 })
 export class SummaryComponent {
   readonly price = input<number>();
+
+  readonly priceInteger = computed(() => {
+    const value = this.price();
+    if (typeof value === 'number' && !isNaN(value)) {
+      return value.toFixed(2).split('.')[0];
+    }
+    return '0';
+  });
+
+  readonly priceFraction = computed(() => {
+    const value = this.price();
+    if (typeof value === 'number' && !isNaN(value)) {
+      return value.toFixed(2).split('.')[1];
+    }
+    return '00';
+  });
 }
